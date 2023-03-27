f236952cf85783b224c0796c1e55bb8b23be58a8
Albert Formenti <albertformenti@gmail.com>
2023-03-27 20:33:25 +0200

    Activity InformacioLegal Feta

--- app/src/main/java/com/example/phf_android/ActivityConfiguracio.java
+++ app/src/main/java/com/example/phf_android/ActivityConfiguracio.java
@@ -2,13 +2,24 @@ package com.example.phf_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityConfiguracio extends AppCompatActivity {
-
+    Button InformacioLegal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracio);
+        InformacioLegal= findViewById(R.id.btnConfiguracioInformacioLegal);
+        InformacioLegal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ActivityConfiguracio.this, InformacioLegal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
