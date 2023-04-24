581003524979f8dfd062e21b53d68ddd06be54f9
eloi planes <hola__eloi@hotmail.com>
2023-04-24 16:05:50 +0200

    Conexio a ddbb y mascotes

--- app/src/main/java/com/example/phf_android/Constants.java
+++ app/src/main/java/com/example/phf_android/Constants.java
@@ -2,4 +2,6 @@ package com.example.phf_android;
 
 public class Constants {
     public static final String MOSTRAR_ANIMALS_ADAPTER = "SELECT `animal`.`imatgePerfil`, `animal`.`nom`, `animal`.`cartilla`, `tipusanimal`.`Nom` FROM `animal` LEFT JOIN `tipusanimal` ON `animal`.`idTipusAnimal` = `tipusanimal`.`idTipusAnimal`;";
+    public static final String AFEGIR_USUARI_REGISTRE = "INSERT INTO `usuaris` (`documentIdentitat`, `nom`, `cognom1`, `cognom2`, `nomUsuari`, `contrasenya`, `correu`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');";
+
 }
